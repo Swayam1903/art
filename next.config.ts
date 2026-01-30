590fe37a99345c40c116211b7c22607e9313ba8c
Swayam1903 <swayambeladiyav@gmail.com>
2026-01-30 15:06:39 +0530

    Add build-time logs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+console.log(">>> [BUILD CHECK] DATABASE_URL is:", process.env.DATABASE_URL ? "FOUND" : "NOT FOUND");
+console.log(">>> [BUILD CHECK] SUPABASE_URL is:", process.env.NEXT_PUBLIC_SUPABASE_URL ? "FOUND" : "NOT FOUND");
+
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
