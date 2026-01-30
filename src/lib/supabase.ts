c964ad4383bf6ffa0f4446d5a0ee6439a8a9ee6d
Swayam1903 <swayambeladiyav@gmail.com>
2026-01-30 13:50:34 +0530

    Fix Vercel build errors

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://lmnwepygaqlwhyuysjdt.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtbndlcHlnYXFsd2h5dXlzamR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NDc3MTYsImV4cCI6MjA4NTMyMzcxNn0.LQqcI59ulk-hpElOiGdvBZX9zbNZn32bDG6jfeB6BdQ';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 if (!supabaseUrl || !supabaseAnonKey) {
     if (process.env.NODE_ENV === 'development') {
